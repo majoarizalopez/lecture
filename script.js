66555c92a55aa5f30b3076d5172e67b5dffdd5d1
majoarizalopez <majoarizalopez@gmail.com>
2024-11-13 12:13:46 -0500

    class

---
+++ script.js
@@ -0,0 +1,62 @@
+var age = 20;
+
+var age;
+
+age = 100;
+
+let username = "Harry";
+username = "Potter";
+let FirstName = "Maria";
+const valuePi = 3.14;
+
+let grade = false;
+let house;
+let house2 = null;
+letage2 = null;
+
+
+// line comment //
+
+
+
+a = a - 5;
+
+let c = 5;
+
+let d = 12;
+
+let e = true;
+
+let f = false;
+
+
+let house;
+
+let number = 100;
+
+if (number != 50){
+house = "The number is Greater than 60";
+console.log(house);
+
+else
+}
+
+else{
+house= "the number is less than 60"
+console.log(house);
+
+}
+
+// b == 5; ==> b = b + 5;
+// b *= 5; ==> b = b * 5;
+
+console.log(age);
+console.log(username);
+console.log(FirstName);
+const valuePi = 3.14;
+console.log(valuePi);
+console.log(grade);
+console.log(house);
+console.log(null);
+console.log(age2);
+console.log(a + b);
